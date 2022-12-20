ab1815f32e1bb00179534a330a4e737921792aa0
Vineet Kumar Srivastava <vineet.iitd@gmail.com>
2022-12-20 13:43:41 +0530

    Added source code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socket = require('socket.io')
+
+var app = express();
+
+var server = app.listen(4000, function() {
+    console.log("Listening on port 4000");
+});
+
+app.use(express.static("public"));
+
+var upgradedServer = socket(server);
+upgradedServer.on("connection", function(socket){
+
+    socket.on("sendingMessage", function (data) {
+        console.log("server received message with ", data);
+        upgradedServer.emit("broadcastMessage", data);
+    });
+
+    console.log("Websocket connected", socket.id);
+})
+
---
+++ public/chat.js
@@ -0,0 +1,19 @@
+var socket = io.connect("http://localhost:4000");
+
+var message = document.getElementById("message");
+var button = document.getElementById("send");
+var username = document.getElementById("username");
+var output = document.getElementById("output");
+
+button.addEventListener("click", function(){
+    socket.emit("sendingMessage", {
+        message: message.value,
+        username: username.value
+    });
+});
+
+socket.on("broadcastMessage", function(data) {
+    console.log("client: received broadcastMessage with ");
+    console.log(data);
+    output.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.message + '</p'; 
+});
